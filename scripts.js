cf5ea30de0578e9761b8a4b5b2727d18fa05a94e
msmarketa <marketa.dragounova@gmail.com>
2021-07-05 15:14:18 +0200

    added animations and scroll effects

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const fadeinElements = document.querySelectorAll('.project');
+
+const elementInView = (element) => {
+  const elementTop = element.getBoundingClientRect().top;
+  
+  return elementTop <= window.innerHeight;
+}
+
+const displayScrollElement = (element) => {
+  element.classList.add('fadein');
+}
+
+const scrollAnimation = () => {
+  fadeinElements.forEach((element) => {
+    if (elementInView(element)) {
+      displayScrollElement(element);
+    }
+  })
+}
+
+window.addEventListener('scroll', () => {
+ scrollAnimation();
+})
