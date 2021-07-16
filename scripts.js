dcb5efba7c3853c2901bac398704dbb41cbc69ee
msmarketa <marketa.dragounova@gmail.com>
2021-07-16 09:45:06 +0200

    prettier setup

--- scripts.js
+++ scripts.js
@@ -1,23 +1,23 @@
-const fadeinElements = document.querySelectorAll('.project');
+const fadeinElements = document.querySelectorAll(".project");
 
 const elementInView = (element) => {
   const elementTop = element.getBoundingClientRect().top;
-  
+
   return elementTop <= window.innerHeight;
-}
+};
 
 const displayScrollElement = (element) => {
-  element.classList.add('fadein');
-}
+  element.classList.add("fadein");
+};
 
 const scrollAnimation = () => {
   fadeinElements.forEach((element) => {
     if (elementInView(element)) {
       displayScrollElement(element);
     }
-  })
-}
+  });
+};
 
-window.addEventListener('scroll', () => {
- scrollAnimation();
-})
+window.addEventListener("scroll", () => {
+  scrollAnimation();
+});
